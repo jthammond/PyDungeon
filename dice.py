bff5525392b6c535240506f03fb28cab3fc3db7c
Gary Daigle <zetaphor@gmail.com>
2016-06-17 18:22:38 -0400

    Cleaned up dice.py indentation

--- dice.py
+++ dice.py
@@ -4,22 +4,22 @@ def diceRoll(max, min=1):
     return randrange(min, max)
 
 def d4():
-     return randrange(1,4)
-     
+    return randrange(1, 4)
+
 def d6():
-     return randrange(1,6)
-     
+    return randrange(1, 6)
+
 def d8():
-     return randrange(1,8)
-    
+    return randrange(1, 8)
+
 def d10():
-     return randrange(1,10)
-    
+    return randrange(1, 10)
+
 def d12():
-     return randrange(1,12)
-     
+    return randrange(1, 12)
+
 def d20():
-     return randrange(1,20)
-     
+    return randrange(1, 20)
+
 def d100():
-    return randrange(1,100)
+    return randrange(1, 100)
