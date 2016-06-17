d5732d4884643f892ba2111e3a42e4c712b573c0
Gary Daigle <zetaphor@gmail.com>
2016-06-17 18:24:54 -0400

    Cleaned up formatting on monsters.py
    Went from using 8 space indentation to 4, broke parameters on monster init onto separate line to conform with PEP8 standard

--- monsters.py
+++ monsters.py
@@ -1,36 +1,40 @@
 import weapons
 
 class Enemy(object):
-        def __init__(self, Name, CR, AC, RM, Wield, AttackPerc, Status):
-                self.Name = Name
-                self.CR = CR #Combat Rating
-                self.AC = AC #Armor Class
-                self.RM = RM #Roll Modifier
-                self.Wield = Wield
-                self.AttackPerc = AttackPerc #Liklihood to attack
-                self.HP = Status['HP']
-                self.INIT = Status['Initiative']
+    def __init__(self, Name, CR, AC, RM, Wield, AttackPerc, Status):
+        self.Name = Name
+        self.CR = CR  # Combat Rating
+        self.AC = AC  # Armor Class
+        self.RM = RM  # Roll Modifier
+        self.Wield = Wield
+        self.AttackPerc = AttackPerc  # Liklihood to attack
+        self.HP = Status['HP']
+        self.INIT = Status['Initiative']
 
 
 class Kobold(Enemy):
-        def __init__(self):
-                self.Name = 'Kobold'
-                self.CR = 0.25
-                self.AC = 14
-                self.RM = 1
-                self.Wield = weapons.Fists
-                self.AttackPerc = 85
-                self.Status = {'HP': 8, 'Initiative' : 1}
+    def __init__(self):
+        self.Name = 'Kobold'
+        self.CR = 0.25
+        self.AC = 14
+        self.RM = 1
+        self.Wield = weapons.Fists
+        self.AttackPerc = 85
+        self.Status = {'HP': 8, 'Initiative': 1}
+
 
 class Rat(Enemy):
-        def __init__(self):
-                self.Name = 'Rat'
-                self.CR = 0.025
-                self.AC = 10
-                self.RM = 0
-                self.Wield = weapons.Fists
-                self.AttackPerc = 8
-                self.Status = {'HP': 4, 'Initiative' : 2}
-                
-kobold = Enemy(Kobold().Name, Kobold().CR, Kobold().AC, Kobold().RM, Kobold().Wield, Kobold().AttackPerc, Kobold().Status)
-rat = Enemy(Rat().Name, Rat().CR, Rat().AC, Rat().RM, Rat().Wield, Rat().AttackPerc, Rat().Status)
+    def __init__(self):
+        self.Name = 'Rat'
+        self.CR = 0.025
+        self.AC = 10
+        self.RM = 0
+        self.Wield = weapons.Fists
+        self.AttackPerc = 8
+        self.Status = {'HP': 4, 'Initiative': 2}
+
+kobold = Enemy(Kobold().Name, Kobold().CR, Kobold().AC, Kobold().RM,
+               Kobold().Wield, Kobold().AttackPerc, Kobold().Status)
+
+rat = Enemy(Rat().Name, Rat().CR, Rat().AC, Rat().RM,
+            Rat().Wield, Rat().AttackPerc, Rat().Status)
