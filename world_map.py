71ebd882d19c5fcc7dc7208281ca921967a9bd6a
Uijoti <uijoti@gmail.com>
2016-07-06 22:29:56 +0000

    Rooms to room in world_map.py

--- world_map.py
+++ world_map.py
@@ -1,16 +1,16 @@
 import json, os
 
-with open(os.path.dirname(os.path.realpath(__file__)) + '/rooms.json') as room_file:
-    rooms = json.load(room_file)
+with open(os.path.dirname(os.path.realpath(__file__)) + '/room.json') as room_file:
+    room = json.load(room_file)
 
-Tutorial = {1:  {1: rooms['WALL'], 2:rooms['WALL'],    3:rooms['WALL'],     4:rooms['WALL'],    5:rooms['WALL'],     6:rooms['WALL'],        7:rooms['WALL'],        8:rooms['WALL'],    9:rooms['WALL']}, # 1
-            2:  {1:rooms['WALL'],  2:rooms['WALL'],    3:rooms['F_ARMORY'], 4:rooms['F_DOJO'],  5:rooms['I_SHOP'],   6:rooms['I_KITCHEN'],   7:rooms['WALL'],        8:rooms['WALL'],    9:rooms['WALL']}, # 2
-            3:  {1:rooms['WALL'],  2:rooms['R_HALL'],  3:rooms['R_ROOM'],   4:rooms['F_GUILD'], 5:rooms['I_BAR'],    6:rooms['I_HALL'],      7:rooms['T_STABLES'],   8:rooms['WALL'],    9:rooms['WALL']}, # 3
-            4:  {1:rooms['WALL'],  2:rooms['R_TRAIN'], 3:rooms['R_GUILD'],  4:rooms['ROAD1'],   5:rooms['T_SQUARE'], 6:rooms['ROAD2'],       7:rooms['ROAD3'],       8:rooms['T_GATES'], 9:rooms['WALL']}, # 4
-            5:  {1:rooms['WALL'],  2:rooms['R_ROOM'],  3:rooms['S_ROOM'],   4:rooms['S_GUILD'], 5:rooms['ROAD4'],    6:rooms['T_MAYOR'],     7:rooms['T_GRAVEYARD'], 8:rooms['WALL'],    9:rooms['WALL']}, # 5
-            6:  {1:rooms['WALL'],  2:rooms['WALL'],    3:rooms['S_TRAIN'],  4:rooms['S_LIB'],   5:rooms['C_GUILD'],  6:rooms['C_ROOM'],      7:rooms['SW_PUZZLE'],   8:rooms['WALL'],    9:rooms['WALL']}, # 6
-            7:  {1:rooms['WALL'],  2:rooms['WALL'],    3:rooms['WALL'],     4:rooms['WALL'],    5:rooms['C_PEWS'],   6:rooms['C_TRAIN'],     7:rooms['SW_MONSTER'],  8:rooms['WALL'],    9:rooms['WALL']}, # 7
-            8:  {1:rooms['WALL'],  2:rooms['WALL'],    3:rooms['WALL'],     4:rooms['WALL'],    5:rooms['C_STAGE'],  6:rooms['C_CONF'],      7:rooms['SW_LOCKED'],   8:rooms['WALL'],    9:rooms['WALL']}, # 8
-            9:  {1:rooms['WALL'],  2:rooms['WALL'],    3:rooms['WALL'],     4:rooms['WALL'],    5:rooms['WALL'],     6:rooms['SW_TREASURE'], 7:rooms['SW_BOSS'],     8:rooms['SW_QI'],   9:rooms['WALL']}, # 9
-            10: {1:rooms['WALL'],  2:rooms['WALL'],    3:rooms['WALL'],     4:rooms['WALL'],    5:rooms['WALL'],     6:rooms['WALL'],        7:rooms['WALL'],        8:rooms['WALL'],    9:rooms['WALL']}  # 10
+Tutorial = {1:  {1: room['WALL'], 2:room['WALL'],    3:room['WALL'],     4:room['WALL'],    5:room['WALL'],     6:room['WALL'],        7:room['WALL'],        8:room['WALL'],    9:room['WALL']}, # 1
+            2:  {1:room['WALL'],  2:room['WALL'],    3:room['F_ARMORY'], 4:room['F_DOJO'],  5:room['I_SHOP'],   6:room['I_KITCHEN'],   7:room['WALL'],        8:room['WALL'],    9:room['WALL']}, # 2
+            3:  {1:room['WALL'],  2:room['R_HALL'],  3:room['R_ROOM'],   4:room['F_GUILD'], 5:room['I_BAR'],    6:room['I_HALL'],      7:room['T_STABLES'],   8:room['WALL'],    9:room['WALL']}, # 3
+            4:  {1:room['WALL'],  2:room['R_TRAIN'], 3:room['R_GUILD'],  4:room['ROAD1'],   5:room['T_SQUARE'], 6:room['ROAD2'],       7:room['ROAD3'],       8:room['T_GATES'], 9:room['WALL']}, # 4
+            5:  {1:room['WALL'],  2:room['R_ROOM'],  3:room['S_ROOM'],   4:room['S_GUILD'], 5:room['ROAD4'],    6:room['T_MAYOR'],     7:room['T_GRAVEYARD'], 8:room['WALL'],    9:room['WALL']}, # 5
+            6:  {1:room['WALL'],  2:room['WALL'],    3:room['S_TRAIN'],  4:room['S_LIB'],   5:room['C_GUILD'],  6:room['C_ROOM'],      7:room['SW_PUZZLE'],   8:room['WALL'],    9:room['WALL']}, # 6
+            7:  {1:room['WALL'],  2:room['WALL'],    3:room['WALL'],     4:room['WALL'],    5:room['C_PEWS'],   6:room['C_TRAIN'],     7:room['SW_MONSTER'],  8:room['WALL'],    9:room['WALL']}, # 7
+            8:  {1:room['WALL'],  2:room['WALL'],    3:room['WALL'],     4:room['WALL'],    5:room['C_STAGE'],  6:room['C_CONF'],      7:room['SW_LOCKED'],   8:room['WALL'],    9:room['WALL']}, # 8
+            9:  {1:room['WALL'],  2:room['WALL'],    3:room['WALL'],     4:room['WALL'],    5:room['WALL'],     6:room['SW_TREASURE'], 7:room['SW_BOSS'],     8:room['SW_QI'],   9:room['WALL']}, # 9
+            10: {1:room['WALL'],  2:room['WALL'],    3:room['WALL'],     4:room['WALL'],    5:room['WALL'],     6:room['WALL'],        7:room['WALL'],        8:room['WALL'],    9:room['WALL']}  # 10
             }
